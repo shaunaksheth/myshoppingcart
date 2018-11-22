7c0f62b11bd5557c7535bad480718ad4374696a4
Shaunak Sheth <shaunak.sheth@gmail.com>
2018-11-22 18:10:17 +0530

    assesment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingitemComponent } from './shoppingitem/shoppingitem.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'items', component: ShoppingitemComponent},
+  {path: 'productlist', component: ProductlistComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path:'**', component: PageNotFoundComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShoppingitemComponent,
+    ProductlistComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent,
+    LogoutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
